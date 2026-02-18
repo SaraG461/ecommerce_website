dfdb169cf70ad4d351bb0415ba6b36701e50327a
Sara Grace <saragrace461@gmail.com>
2026-02-18 14:25:16 +0000

    fix banner and background

--- src/comp/home.js
+++ src/comp/home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './home.css'
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
     <>
     <div className='home'>
       <div className='top_banner'>
+        
+        <div className = 'contant'>
+          <h3>Cottage core</h3>
+        <h2>Cozy & Pretty</h2>
+        <p>25% off at your first purchase!</p>
+        <Link to="/">Shop Now</Link>
+
+        </div>
+        
 
       </div>
     </div>
